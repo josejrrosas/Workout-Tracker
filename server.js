3e7d16153b9c3be8ae802e3148b6d50e890257a2
Jose Rosas <josejrrosas@yahoo.com>
2021-09-09 18:45:28 -0500

    deploying

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ mongoose.connect(
   }
 );
 
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.get("/exercise", (req, res) => {
 	res.sendFile(path.join(__dirname, "./public/exercise.html"));
 });
